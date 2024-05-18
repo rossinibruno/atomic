de2394b9a43788e461add16b96c652da9ab9ac35
Bruno Erick Rossini <bruno.rossini@BAPBR-C02GV02Z0KPF.local>
2024-05-18 15:19:15 -0300

    feat: alive

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as fs from 'fs';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  // const httpsOptions = {
+  //   key: fs.readFileSync('./secrets/private-key.pem'),
+  //   cert: fs.readFileSync('./secrets/public-certificate.pem'),
+  // };
+
+  // const httpsOptions = {
+  //   cert: fs.readFileSync(''), // Certificado fullchain do dominio
+  //   key: fs.readFileSync('.'), // Chave privada do domínio
+  //   ca: fs.readFileSync('./certificates/chain-pix-sandbox.crt'), // Certificado público da Efí
+  //   minVersion: 'TLSv1.2',
+  //   requestCert: true,
+  //   rejectUnauthorized: true, //Caso precise que os demais endpoints não rejeitem requisições sem mTLS, você pode alterar para false
+  // };
+
+  const app = await NestFactory.create(AppModule, {
+    // httpsOptions,
+  });
   await app.listen(3000);
 }
 bootstrap();
